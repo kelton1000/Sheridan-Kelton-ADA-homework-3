5681cba5d2edc0f0dece85d13df69ddcc771a5b3
Kelton <keltonsheridan@gmail.com>
2020-05-04 20:14:55 -0600

    third

--- Sheridan-Kelton-ADA-homework-3.R
+++ Sheridan-Kelton-ADA-homework-3.R
@@ -1,8 +1,12 @@
 library(ggplot2)
 
-f <- file.choose()
 d <- read.csv("/Users/kelton/Desktop/ADA/KamilarAndCooper.csv")
 d
 
 ggplot(data = d, aes(y = MaxLongevity_m, x = Brain_Size_Species_Mean)) + 
-  geom_point() + geom_smooth(method = "lm")
+  geom_point() + geom_smooth(method = "lm", se = FALSE)
+
+m <- lm(formula = MaxLongevity_m ~ Brain_Size_Species_Mean, data = d)
+m2 <- lm(formula = log(MaxLongevity_m) ~ log(Brain_Size_Species_Mean), data = d)
+confint(m, level = 0.90)
+confint(m2, level = 0.90)
